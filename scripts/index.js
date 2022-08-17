19c157540873776a59e79576bec0956fe3e5ec8e
Sergei Pankov <sergeipankov98@mail.ru>
2022-08-17 13:37:47 +0400

    Fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,10 @@
 // Кнопки
 let profileEditButton = document.querySelector(".profile__edit-button");
 let popupClose = document.querySelector(".popup__close");
-
-profileEditButton.addEventListener("click", profilePopupChange); 
-popupClose.addEventListener("click", profilePopupChange); 
-
-
 let editSubmit = document.querySelector(".popup__form");
-
-editSubmit.addEventListener("submit", profileSubmit);
-
-
-//Окно ПопАп-а
 let profileEditPopup = document.querySelector(".popup");
-
-//Имя и описание профиля
-
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
-
 
 function profilePopupChange(){
     profileEditPopup.classList.toggle("popup_opened");
@@ -36,3 +22,7 @@ function profileSubmit(evt){
     profilePopupChange();
 
 }
+
+profileEditButton.addEventListener("click", profilePopupChange); 
+popupClose.addEventListener("click", profilePopupChange); 
+editSubmit.addEventListener("submit", profileSubmit);
