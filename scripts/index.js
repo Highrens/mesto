e5c359bbd9dacd33cadf2572ebf3bb9fa5c9daec
Sergei Pankov <sergeipankov98@mail.ru>
2022-08-19 23:46:12 +0400

    First big Fixes

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,14 @@ let profileEditButton = document.querySelector(".profile__edit-button");
 let popupClose = document.querySelector(".popup__close");
 let editSubmit = document.querySelector(".popup__form");
 let profileEditPopup = document.querySelector(".popup");
+
+// Имя и описание профиля
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
+
+// Поля ввода 
+let newProfileName = document.querySelector(".popup__text_type_name");
+let newProfileDescription = document.querySelector(".popup__text_type_about");
 
 function profilePopupChange(){
     profileEditPopup.classList.toggle("popup_opened");
@@ -13,16 +19,22 @@ function profilePopupChange(){
 
 function profileSubmit(evt){
     evt.preventDefault();
+    
+    profileName.textContent          = newProfileName.value;
+    profileDescription.textContent   = newProfileDescription.value;
 
-    let newProfileName = document.querySelector(".popup__text_type_name");
-    let newProfileDescription = document.querySelector(".popup__text_type_about");
-
-    profileName.innerHTML = newProfileName.value;
-    profileDescription.textContent = newProfileDescription.value;
     profilePopupChange();
 
 }
 
-profileEditButton.addEventListener("click", profilePopupChange); 
+
+function popupOpen(){
+    newProfileName.value            = profileName.textContent;
+    newProfileDescription.value     = profileDescription.textContent;
+
+    profileEditPopup.classList.toggle("popup_opened");
+}
+
+profileEditButton.addEventListener("click", popupOpen); 
 popupClose.addEventListener("click", profilePopupChange); 
 editSubmit.addEventListener("submit", profileSubmit);
