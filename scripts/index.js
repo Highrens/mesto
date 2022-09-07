795cf81c77b885655f4791c40257847b1b4c9719
Sergei Pankov <sergeipankov98@mail.ru>
2022-09-07 15:16:25 +0400

    Add 3 feac

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,47 @@
-
 // Кнопки
-let profileEditButton = document.querySelector(".profile__edit-button");
-let popupClose = document.querySelector(".popup__close");
-let editSubmit = document.querySelector(".popup__form");
-let profileEditPopup = document.querySelector(".popup");
+
+let popupClose = document.querySelectorAll(".popup__close"); //Кнопка закрытия Всех попапов, раз они не открываются одновременно
+
+    // Настройка профиля
+let profileEditButton = document.querySelector(".profile__edit-button"); //Кнопка Открытия
+let editSubmit = document.querySelector("[name=profile-edit-form]"); //Форма
+let profileEditPopup = document.querySelector(".popup-profile-edit"); // Попап
+
+    // Добавление фотокарточки
+let AddElementButton = document.querySelector(".profile__add-button")
+let AddElemetSubmit = document.querySelector("[name=add-element-form]")
+let AddElementPopup = document.querySelector(".popup-add-element")
 
 // Имя и описание профиля
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 
 // Поля ввода 
-let newProfileName = document.querySelector(".popup__text_type_name");
-let newProfileDescription = document.querySelector(".popup__text_type_about");
+    //
+    let newProfileName = document.querySelector(".popup__text_type_name");
+    let newProfileDescription = document.querySelector(".popup__text_type_about");
+    //
+    let NewElementName = document.querySelector(".popup__text_type_element_name");
+    let NewElementSrc = document.querySelector(".popup__text_type_element_src");
 
-function profilePopupClose(){
-    profileEditPopup.classList.remove("popup_opened");
+//Функция ищет и закрывает открытый попап
+function PopupClose(){
+    let openedPopup = document.querySelector(".popup_opened");
+    openedPopup.classList.remove("popup_opened");
+}
+
+
+function profileEditPopupOpen(){
+    newProfileName.value            = profileName.textContent;
+    newProfileDescription.value     = profileDescription.textContent;
+
+    profileEditPopup.classList.add("popup_opened");
+}
+
+function AddElementPopupOpen(){
+    AddElementPopup.classList.add("popup_opened");
+    NewElementName.value = '';
+    NewElementSrc.value = '';
 }
 
 function profileSubmit(evt){
@@ -23,18 +50,92 @@ function profileSubmit(evt){
     profileName.textContent          = newProfileName.value;
     profileDescription.textContent   = newProfileDescription.value;
 
-    profilePopupClose();
-
+    PopupClose();
 }
 
 
-function popupOpen(){
-    newProfileName.value            = profileName.textContent;
-    newProfileDescription.value     = profileDescription.textContent;
+function AddElement(evt){
+    evt.preventDefault();
 
-    profileEditPopup.classList.add("popup_opened");
+    const ElementTemplate = document.querySelector('#element').content;
+    let Elements = document.querySelector(".elements");
+    let NewElement = ElementTemplate.querySelector('.element').cloneNode(true);
+
+    NewElement.querySelector(".element__name").textContent = NewElementName.value;
+    NewElement.querySelector(".element__image").src = NewElementSrc.value;
+    NewElement.querySelector('.element__like').addEventListener('click', function(evt){
+        evt.target.classList.toggle('element__like_active');
+      })
+      NewElement.querySelector('.element__delete').addEventListener('click', function(evt){
+        evt.target.parentElement.remove();
+      })
+
+    Elements.prepend(NewElement);
+
+    PopupClose();
 }
 
-profileEditButton.addEventListener("click", popupOpen); 
-popupClose.addEventListener("click", profilePopupClose); 
+// Слушаем кнопки
+popupClose.forEach(function (Close){
+    Close.addEventListener("click", PopupClose)
+})
+
+
+profileEditButton.addEventListener("click", profileEditPopupOpen); 
 editSubmit.addEventListener("submit", profileSubmit);
+
+AddElementButton.addEventListener("click", AddElementPopupOpen);
+AddElemetSubmit.addEventListener("submit", AddElement);
+
+
+// Первые 6 карточек 
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+function AddStartCards (CardsArray){
+
+    const ElementTemplate = document.querySelector('#element').content;
+
+    let Elements = document.querySelector(".elements");
+
+    CardsArray.forEach(element => {
+        let NewElement = ElementTemplate.querySelector('.element').cloneNode(true);
+
+    NewElement.querySelector(".element__name").textContent = element.name;
+    NewElement.querySelector(".element__image").src = element.link;
+    NewElement.querySelector('.element__like').addEventListener('click', function(evt){
+        evt.target.classList.toggle('element__like_active');
+      })
+    NewElement.querySelector('.element__delete').addEventListener('click', function(evt){
+        evt.target.parentElement.remove();
+      })
+    Elements.prepend(NewElement);
+    });
+    
+}
+AddStartCards(initialCards);
